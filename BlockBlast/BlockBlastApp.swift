e2dd21583ee77269ce056ef9daa90b10c2c54eff
Aadharsh Rajkumar <aadharsh.2020@gmail.com>
2025-12-15 13:26:55 -0800

    Initial BlockBlast Template

--- BlockBlast/BlockBlastApp.swift
+++ BlockBlast/BlockBlastApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BlockBlastApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BlockBlast_ContentView()
         }
     }
 }
--- BlockBlast/BlockBlast_ContentView.swift
+++ BlockBlast/BlockBlast_ContentView.swift
@@ -1,13 +1,13 @@
 //
-//  ContentView.swift
-//  BlockBlast
+//  BlockBlast_ContentView.swift
+//  Cravr BlockBlast
 //
 //  Created by Aadharsh Rajkumar on 12/15/25.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct BlockBlast_ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    BlockBlast_ContentView()
 }
